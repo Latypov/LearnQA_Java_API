7d18fa7d28dc3a08f7000ffb56b0a0ec1ff70350
Allan <nurulla@gmail.com>
2023-06-20 21:41:47 -0400

    HelloSomeone

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,11 +1,15 @@
 package test.java;
-
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
     @Test
     public void testHelloWorld(){
-        System.out.println("Hello from Allan!");
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
     }
 }
